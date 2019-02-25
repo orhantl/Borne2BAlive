f25824b4a48e7ee6e766165108e6222fc9d934f3
weFlowAndFlow <weflowandflow@icloud.com>
2019-02-25 14:32:07 +0100

    creation projet

---
+++ Borne2BAlive/Borne2BAlive-ejb/src/java/tests/DataTest.java
@@ -0,0 +1,21 @@
+
+package tests;
+
+public class DataTest {
+
+    public static void main(String[] args) {
+        
+        
+        
+        // data test here
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
